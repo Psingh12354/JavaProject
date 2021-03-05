54de9e679db50e287098ab4ad7dcfd984733f79a
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2021-03-05 10:05:17 +0530

    Create Assg1Q4.java

---
+++ Assg1Q4.java
@@ -0,0 +1,15 @@
+
+public class Assg4 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char itm1='s';
+		char itm2='e';
+		if(itm1>itm2){
+			System.out.println(itm2+" "+itm1);
+		}
+		else{
+			System.out.println(itm1+" "+itm2);
+		}
+	}
+
+}
