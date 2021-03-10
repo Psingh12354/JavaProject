483a33a1cb7f6b19d9c669f9e93fc06e23d14711
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2021-03-10 12:27:09 +0530

    Create ProtectedAccessSpecifier.java

---
+++ ProtectedAccessSpecifier.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    A obj=new A();
+	    obj.display();
+	}
+}
+class A{
+    protected void display(){
+        System.out.println("A");
+    }
+}
